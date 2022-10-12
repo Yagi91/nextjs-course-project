e4f2eeefac854e36c0aa7696928e9b704e243f25
Yagi91 <bryantimah@gmail.com>
2022-10-12 18:57:45 +0100

    fix fallback and set it to blocking

--- pages/index.js
+++ pages/index.js
@@ -62,7 +62,7 @@ export async function getStaticProps() {
         id: meetup._id.toString(),
       })),
     },
-    revalidate: 10,
+    revalidate: 1,
   };
 }
 // export async function getServerSideProps(context){
